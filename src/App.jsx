cf2e0568230f59f68c6b4fb12c10beb54bc19861
I an <ydh8485@gmail.com>
2021-05-09 23:54:37 +0900

    project setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+// {}, import 대상이 default가 아님(export default)
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia,CssBaseline, Grid, Toolbar, Container} from '@material-ui/core';
+//  icon
+import { PhotoCamera } from '@material-ui/icons';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+    {/* provide default styling */}
+      <CssBaseline />
+      {/* navigation bar */}
+      <AppBar position="relative">
+        {/* 도구모음, 파란선 */}
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            {/* 제목 부분 */}
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Photo Album
+            </Typography>
+            {/* 설명 */}
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Hello everyone This is a photo album adn i'm trying to make this sentence as long as possible so we can see how does it look like on the screen
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
   )
 }
 
 export default App;
+
+// meterial-ui.com
+// 데모, 소스, 설명 : component
